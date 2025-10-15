6d357bed940ca729a6a2f879e10e6f912c0c6c0d
Jaeseok <jojs98@naver.com>
2025-10-15 23:20:29 +0900

    Change the path of the image to the server commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import path from 'path';
 import 'express-async-errors';
 import restaurantsRouter from './router/restaurants.js';
 import userRouter from './router/users.js';
@@ -15,6 +16,19 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
+
+app.use(
+  '/uploads',
+  helmet.crossOriginResourcePolicy({ policy: 'cross-origin' })
+);
+
+app.use(
+  '/uploads',
+  express.static(path.join(process.cwd(), 'uploads'), {
+    maxAge: '7d',
+    immutable: true,
+  })
+);
 
 app.use('/restaurants', restaurantsRouter);
 app.use('/users', userRouter);
