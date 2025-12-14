575f3acac69a3f6a47272c180c833703d71ebd90
Jaeseok <jojs98@naver.com>
2025-12-14 21:35:20 +0900

    Update get me api

--- controller/user.js
+++ controller/user.js
@@ -187,7 +187,7 @@ export async function me(req, res) {
   if (!user) {
     return res.status(404).json({ message: 'User not found' });
   }
-  res.status(200).json({ username: user.username });
+  res.status(200).json({ id: user.id, username: user.username });
 }
 
 export async function getMyProfile(req, res) {
